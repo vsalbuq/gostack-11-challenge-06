e750208a4f91d99c60c0acea644483a2f600ebf4
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2020-09-12 17:00:40 -0300

    feat: Import page done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import { format } from 'path';
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
 import total from '../../assets/total.svg';
